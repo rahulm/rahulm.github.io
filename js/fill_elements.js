aec57d1f41225ff90d797c57626def1e386e8346
Rahul Malavalli <rahul.malavalli@gmail.com>
2018-01-05 17:44:05 -0800

    Made work experience section with reusable template.

--- js/fill_elements.js
+++ js/fill_elements.js
@@ -25,18 +25,42 @@ var experienceList = [
     "workplace" : "PlayFull",
     "position" : "Lead Android Developer",
     "duration" : "October 2014 - September 2015"
-  },
+  }
 ];
 
 function fillExperience() {
+  var workexptemplatestr = document.getElementById("infotemplate");
   var workexpdiv = document.getElementById("workexperiencediv");
-  console.log(workexpdiv.innerHTML);
+  var currRow = null;
+  
+  for (i = 0; i < experienceList.length; ++i) {
+    var tmp = document.createRange().createContextualFragment(workexptemplatestr.innerHTML);
+    var exp = experienceList[i];
+    tmp.getElementById("image").src = exp["image"]["src"];
+    tmp.getElementById("image").alt = exp["image"]["alt"];
+    
+    tmp.getElementById("firsttitle").innerHTML = exp["workplace"];
+    tmp.getElementById("secondtitle").innerHTML = exp["position"];
+    tmp.getElementById("thirdtitle").innerHTML = exp["duration"];
+    
+    if ((i % 3) == 0) {
+      if (currRow != null) {
+        workexpdiv.appendChild(currRow);
+      }
+      currRow = document.createElement("div");
+      currRow.classList.add("row");
+    }
+    currRow.appendChild(tmp);
+  }
+  
+  workexpdiv.appendChild(currRow);
+  
   return 0;
 }
 
 function fillProjects() {
-  var projectsdiv = document.getElementById("projectsdiv");
-  console.log(projectsdiv.innerHTML);
+  // var projectsdiv = document.getElementById("projectsdiv");
+  // console.log(projectsdiv.innerHTML);
   return 0;
 }
 
