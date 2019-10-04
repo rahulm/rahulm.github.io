ce472a8bac78f59b9ee839137d0c8eed78ae2c99
Rahul Malavalli <rahul.malavalli@gmail.com>
2019-10-03 17:59:23 -0700

    added descriptions to all items

--- js/dataset.js
+++ js/dataset.js
@@ -7,7 +7,8 @@ var experienceList = [
     "line1" : "Facebook",
     "line2" : "Software Engineering Intern",
     "line3" : "June 2018 - September 2018",
-    "textarea" : ""
+    "textarea" : `- Backend development on storage/databases team for Messenger Infrastructure.
+- Developed internal tool forrecovery of content in disaster scenarios.`
   },
   {
     "image" : {
@@ -17,7 +18,12 @@ var experienceList = [
     "line1" : "BuildUCLA Collections Lab",
     "line2" : "Student Researcher",
     "line3" : "January 2018 - June 2018",
-    "textarea" : ""
+    "textarea" : `- Trained models and developed computer vision pipeline to detect annotations/marginalia, of interest to researchers, in digital copies of old books and manuscripts.
+- Performed preprocessing and sampling on dataset of digitized books from the UCLA library.
+- Implemented Convolutional Neural Networks (CNNs) in PyTorch to perform object recognition and detection tasks on books.`,
+    "links" : [
+      "https://github.com/collectionslab/Omniscribe"
+    ]
   },
   {
     "image" : {
@@ -27,7 +33,11 @@ var experienceList = [
     "line1" : "Google",
     "line2" : "Software Engineering Intern",
     "line3" : "June 2017 - September 2017",
-    "textarea" : ""
+    "textarea" : `- Analyzed Android Instant App memory footprints and visibility, among other
+factors, to determine causes of instant app crashes in low memory situations.
+- Implemented instant app process management system in Java to gracefully
+manage life cycles of Android Instant Apps and ensure reliably smooth user
+experience.`
   },
   {
     "image" : {
@@ -37,7 +47,12 @@ var experienceList = [
     "line1" : "SARP Lab - UCLA Health",
     "line2" : "Undergrad CS Researcher",
     "line3" : "April 2016 - September 2016",
-    "textarea" : ""
+    "textarea" : `- Use of smart devices in allowing physicians to remotely monitor patient health and activity, primarily in the geriatric population.
+- Trained machine learning algorithms (using scikit-learn in Python) to predict positions and activities of patients from raw sensor data, improving precision and recall on poorly performing activities by nearly 2.5 fold.`,
+    "links" : [
+      "http://risksciences.ucla.edu/smart-health/",
+      "http://risksciences.ucla.edu/current-projects/2016/8/8/sarp-sensing-at-risk-populations"
+    ]
   },
   {
     "image" : {
@@ -47,7 +62,14 @@ var experienceList = [
     "line1" : "PlayFull",
     "line2" : "Lead Android Developer",
     "line3" : "October 2014 - September 2015",
-    "textarea" : ""
+    "textarea" : `- Worked with local startup (~6 members) as lead Android developer building a service to reward users playing games on the PlayFull application with discounts at restaurants.
+- Fully redesigned and developed new version of PlayFull Android application from ground up.
+- Built a Snake game variant within the PlayFull Android application.
+- Developed in-app features increasing Android installs from less than 100 to over 500.
+- Member of the PlayFull LA Hacks 2015 Sponsor Team, providing major catering for all hackers participating.`,
+    "links" : [
+      "https://beplayfull.com"
+    ]
   }
 ];
 
@@ -60,7 +82,10 @@ var projectList = [
     "line1" : "Analysis of VAEs for Reconstructive and Generative Tasks",
     "line2" : "ECE232AS Final Project",
     "line3" : "March 2019",
-    "textarea" : ""
+    "textarea" : `- Development (in PyTorch) and comparison of Autoencoders (AEs) and Variational Autoencoders (VAEs) as generative models on the MNIST and Eigenfaces datasets.`,
+    "links" : [
+      "https://github.com/rahulm11/ee239as-project"
+    ]
   },
   {
     "image" : {
@@ -70,7 +95,17 @@ var projectList = [
     "line1" : "FPGA Depth Perception",
     "line2" : "CS 152B Final Project",
     "line3" : "October 2017 - December 2017",
-    "textarea" : ""
+    "textarea" : `- Custom final project designed and developed during UCLA's CS 152B course.
+- As an initial project in basic computer vision, experimented with depth perception on Field Programmable Gate Array (FPGA) with stereo camera peripheral.
+- Performed basic depth perception with low resolution and low quality camera input on limited hardware from FPGA.
+- Optimized Sum of Absolute Differences (SAD) algorithm to run on FPGA.
+
+Credit also goes to my two partners:
+- Jeff Au-Yeung
+- Naing Min`,
+    "links" : [
+      "https://github.com/rahulm11/depth-perception-fpga"
+    ]
   },
   {
     "image" : {
@@ -80,7 +115,13 @@ var projectList = [
     "line1" : "Indoor Positioning through Machine Learning on WiFi Fingerprints",
     "line2" : "Research Paper - IPIN 2017",
     "line3" : "February 2017 - September 2017",
-    "textarea" : ""
+    "textarea" : `- Trained machine learning algorithm on ambient WiFi RSSI values to achieve F-measures approaching and far above 0.9 in university and home environments.
+- Integrated model into Android application for live training and prediction.
+- Paper written with two partners accepted into Indoor Positioning and Indoor Navigation (IPIN) 2017 conference in WIP division.`,
+    "links" : [
+      "https://github.com/arjun372/Indoor-WiFi-Localizer",
+      "http://www.ipin2017.org/ipinpapers/224/224.pdf"
+    ]
   },
   {
     "image" : {
@@ -90,7 +131,12 @@ var projectList = [
     "line1" : "Android Knock Unlock",
     "line2" : "EE3 Final Project",
     "line3" : "April 2016 - June 2016",
-    "textarea" : ""
+    "textarea" : `- Developed a door unlocking system triggered by a secret knock code set by the user.
+- Gained experience in basic circuit design (breadboard-based) and interfacing of microcontrollers with peripherals (such as buttons and motors).
+- Use of Arduino 101 to store and detect the knock sequence from on-board accelerometer input, and to control the entire system.`,
+    "links" : [
+      "https://youtu.be/mg_SPeKW2Tc"
+    ]
   },
   {
     "image" : {
@@ -100,7 +146,12 @@ var projectList = [
     "line1" : "PorFavor",
     "line2" : "LAHacks 2016 Project",
     "line3" : "April 2016",
-    "textarea" : ""
+    "textarea" : `- Developed an app/service to connect people in need of favors with those in the general public who can help them.
+- Manages transactions for favor requests and bids among all parties involved.
+- Basic use of Ionic framework, in addition to a back end infrastructure, to simultaneously build apps for multiple platforms.`,
+    "links" : [
+      "http://devpost.com/software/porfavor"
+    ]
   },
   {
     "image" : {
@@ -110,7 +161,14 @@ var projectList = [
     "line1" : "PoliSense",
     "line2" : "Hacktech (Caltech Hackathon) 2016 Project",
     "line3" : "February 2016",
-    "textarea" : ""
+    "textarea" : `- Project completed and submitted for the 2016 Hacktech competition (Caltech Hackathon).
+- Created a web app to gauge and compare the public sentiment (from Twitter) to the political candidates of the 2016 U.S. Presidential Race, and visualize how those feelings have changed over time due to debates, news stories, etc.
+- Utilized Microsoft Azure services (cloud storage, web hosting, machine learning server).
+- Learned basic front-end development using Node.js and multiple libraries using the npm (node.js package manager).`,
+    "links" : [
+      "https://github.com/andrew-kennedy/hacktech-2016",
+      "http://devpost.com/software/hacktech-2016"
+    ]
   },
   {
     "image" : {
@@ -120,7 +178,7 @@ var projectList = [
     "line1" : "UCLA Dining App",
     "line2" : "Android Application",
     "line3" : "August 2015",
-    "textarea" : ""
+    "textarea" : `I developed a simple and easy to use Android app that solved a problem I encountered every day; I wanted a better and faster way to access UCLA dining menus. So, I created a library that parsed one of the UCLA dining menu websites and created an Android app that displayed dining information in a clean and responsive UI.`
   },
   {
     "image" : {
@@ -130,7 +188,8 @@ var projectList = [
     "line1" : "amaze",
     "line2" : "Android Game",
     "line3" : "July 2014 - August 2014",
-    "textarea" : ""
+    "textarea" : `- Learned Android development and basic game design principles using the libGDX game engine.
+- Designed and developed a simple new game from scratch, publishing a complete game on Google Play.`
   }
 ];
 
