d555c5a8958bbda1ef27e09ea686e0ef1613aa41
Rahul Malavalli <rahul.malavalli@gmail.com>
2020-12-27 14:19:54 -0800

    Changed spaces from 2 to 4

--- js/fillElements.js
+++ js/fillElements.js
@@ -1,36 +1,36 @@
 
 function fillTemplate(templateElementName, targetDivName, dataName) {
-  var projectstemplatestr = document.getElementById(templateElementName);
-  var targetdiv = document.getElementById(targetDivName);
-  var data = dataDict[dataName]
-  var currRow = null;
-  
-  for (i = 0; i < data.length; ++i) {
-    var tmp = document.createRange().createContextualFragment(projectstemplatestr.innerHTML);
-    var box = tmp.getElementById("infotemplatebox");
-    var exp = data[i];
-    
-    box.dataset.infoType = dataName;
-    box.dataset.infoId = i;
-    
-    tmp.getElementById("infotemplatebox-image").src = exp["image"]["src"];
-    tmp.getElementById("infotemplatebox-image").alt = exp["image"]["alt"];
-    
-    tmp.getElementById("infotemplatebox-line1").innerHTML = exp["line1"];
-    tmp.getElementById("infotemplatebox-line2").innerHTML = exp["line2"];
-    tmp.getElementById("infotemplatebox-line3").innerHTML = exp["line3"];
-    
-    if ((i % 3) == 0) {
-      if (currRow != null) {
-        targetdiv.appendChild(currRow);
-      }
-      currRow = document.createElement("div");
-      currRow.classList.add("row");
+    var projectstemplatestr = document.getElementById(templateElementName);
+    var targetdiv = document.getElementById(targetDivName);
+    var data = dataDict[dataName]
+    var currRow = null;
+
+    for (i = 0; i < data.length; ++i) {
+        var tmp = document.createRange().createContextualFragment(projectstemplatestr.innerHTML);
+        var box = tmp.getElementById("infotemplatebox");
+        var exp = data[i];
+
+        box.dataset.infoType = dataName;
+        box.dataset.infoId = i;
+
+        tmp.getElementById("infotemplatebox-image").src = exp["image"]["src"];
+        tmp.getElementById("infotemplatebox-image").alt = exp["image"]["alt"];
+
+        tmp.getElementById("infotemplatebox-line1").innerHTML = exp["line1"];
+        tmp.getElementById("infotemplatebox-line2").innerHTML = exp["line2"];
+        tmp.getElementById("infotemplatebox-line3").innerHTML = exp["line3"];
+
+        if ((i % 3) == 0) {
+            if (currRow != null) {
+                targetdiv.appendChild(currRow);
+            }
+            currRow = document.createElement("div");
+            currRow.classList.add("row");
+        }
+        currRow.appendChild(tmp);
     }
-    currRow.appendChild(tmp);
-  }
-  
-  targetdiv.appendChild(currRow);
+
+    targetdiv.appendChild(currRow);
 }
 
 fillTemplate("infotemplatescript", "workexperiencediv", "experience");
@@ -40,76 +40,76 @@ fillTemplate("infotemplatescript", "projectsdiv", "projects");
 // history.pushState(modalState, "", "");
 var modalStatePushed = false;
 
-$(document).on("show.bs.modal","#descriptionModal", function (event) {
-  // start modal back button
-  if (modalStatePushed == true) {
-    history.replaceState(null, "", "");
-  } else {
-    history.pushState(null, "", "");
-    modalStatePushed = true;
-  }
-  // end modal back button
-  
-  
-  var triggerBox = $(event.relatedTarget);
-  var d = dataDict[triggerBox.data("infoType")][triggerBox.data("infoId")];
-  
-  var modal = $(this);
-  modal.find("#descriptionModal-image").attr("src", d["image"]["src"]);
-  modal.find("#descriptionModal-image").attr("alt", d["image"]["alt"]);
-  
-  modal.find("#descriptionModal-line1").text(d["line1"]);
-  modal.find("#descriptionModal-line2").text(d["line2"]);
-  modal.find("#descriptionModal-line3").text(d["line3"]);
-  
-  // var descr = "<ul>";
-  // descr += d["textarea"].replace(/\- /g, "<li>").replace(/\n/g, "</li>");
-  // descr += "</ul>";
-  // modal.find("#descriptionModal-textarea").html(descr);
-  
-  
-  if ("textarea" in d) {
-    var val = d["textarea"].replace(/\n/g, "<br>");
-    modal.find("#descriptionModal-textarea-box").css("display", "block");
-    modal.find("#descriptionModal-textarea").html(val);
-  } else {
-    modal.find("#descriptionModal-textarea-box").css("display", "none");
-    modal.find("#descriptionModal-textarea").text("");
-  }
-  
-  
-  if ("links" in d) {
-    var links = d["links"];
-    var vals = [];
-    for (i = 0; i < links.length; ++i) {
-      vals.push(`<a href="${links[i]}">${links[i]}</a>`);
+$(document).on("show.bs.modal", "#descriptionModal", function (event) {
+    // start modal back button
+    if (modalStatePushed == true) {
+        history.replaceState(null, "", "");
+    } else {
+        history.pushState(null, "", "");
+        modalStatePushed = true;
     }
-    var val = vals.join("<br>");
-    modal.find("#descriptionModal-links-box").css("display", "block");
-    modal.find("#descriptionModal-links").html(val);
-  } else {
-    modal.find("#descriptionModal-links-box").css("display", "none");
-    modal.find("#descriptionModal-links").text("");
-  }
+    // end modal back button
+
+
+    var triggerBox = $(event.relatedTarget);
+    var d = dataDict[triggerBox.data("infoType")][triggerBox.data("infoId")];
+
+    var modal = $(this);
+    modal.find("#descriptionModal-image").attr("src", d["image"]["src"]);
+    modal.find("#descriptionModal-image").attr("alt", d["image"]["alt"]);
+
+    modal.find("#descriptionModal-line1").text(d["line1"]);
+    modal.find("#descriptionModal-line2").text(d["line2"]);
+    modal.find("#descriptionModal-line3").text(d["line3"]);
+
+    // var descr = "<ul>";
+    // descr += d["textarea"].replace(/\- /g, "<li>").replace(/\n/g, "</li>");
+    // descr += "</ul>";
+    // modal.find("#descriptionModal-textarea").html(descr);
+
+
+    if ("textarea" in d) {
+        var val = d["textarea"].replace(/\n/g, "<br>");
+        modal.find("#descriptionModal-textarea-box").css("display", "block");
+        modal.find("#descriptionModal-textarea").html(val);
+    } else {
+        modal.find("#descriptionModal-textarea-box").css("display", "none");
+        modal.find("#descriptionModal-textarea").text("");
+    }
+
+
+    if ("links" in d) {
+        var links = d["links"];
+        var vals = [];
+        for (i = 0; i < links.length; ++i) {
+            vals.push(`<a href="${links[i]}">${links[i]}</a>`);
+        }
+        var val = vals.join("<br>");
+        modal.find("#descriptionModal-links-box").css("display", "block");
+        modal.find("#descriptionModal-links").html(val);
+    } else {
+        modal.find("#descriptionModal-links-box").css("display", "none");
+        modal.find("#descriptionModal-links").text("");
+    }
 });
 
-$('div.modal').on('show', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
+$('div.modal').on('show', function () {
+    var modal = this;
+    var hash = modal.id;
+    window.location.hash = hash;
+    window.onhashchange = function () {
+        if (!location.hash) {
+            $(modal).modal('hide');
+        }
+    }
 });
 
-$(window).on('popstate', function(event) {
-  if (($("#descriptionModal").data('bs.modal') || {})._isShown) {
-    modalStatePushed = false;
-    $("#descriptionModal").modal("hide");
-  } else if (modalStatePushed == true) {
-    history.back();
-  }
+$(window).on('popstate', function (event) {
+    if (($("#descriptionModal").data('bs.modal') || {})._isShown) {
+        modalStatePushed = false;
+        $("#descriptionModal").modal("hide");
+    } else if (modalStatePushed == true) {
+        history.back();
+    }
 });
 
--- js/pickAboutMe.js
+++ js/pickAboutMe.js
@@ -1,40 +1,40 @@
 // pickAboutMe.js
 
 ABOUT_ME_LINES = [
-  "I'm Rahul, the restless.",
-  "I'm Rahul, the rambunctious.",
-  "The sole San Diegan sans surfing skills.",
-  "A determined disciple of the digital domain.",
-  "A connoisseur of chips, both computed and consumed.",
-  "The name is Malavalli. Rahul Malavalli.",
-  "An aspiring artist of ambiguous ability.",
-  "An eager enthusiast of electronics."
+    "I'm Rahul, the restless.",
+    "I'm Rahul, the rambunctious.",
+    "The sole San Diegan sans surfing skills.",
+    "A determined disciple of the digital domain.",
+    "A connoisseur of chips, both computed and consumed.",
+    "The name is Malavalli. Rahul Malavalli.",
+    "An aspiring artist of ambiguous ability.",
+    "An eager enthusiast of electronics."
 ];
 
 
 function pickAboutMe(lines, targetId) {
-  var numLines = lines.length;
-  if (numLines > 0) {
-    var sel = Math.floor((Math.random() * numLines));
-    var item = document.getElementById(targetId);
-    
-    transitionEndCallback = function(e) {
-      item.removeEventListener("transitionend", transitionEndCallback);
-      item.innerHTML = lines[sel];
-      item.classList.toggle("fade");
+    var numLines = lines.length;
+    if (numLines > 0) {
+        var sel = Math.floor((Math.random() * numLines));
+        var item = document.getElementById(targetId);
+
+        transitionEndCallback = function (e) {
+            item.removeEventListener("transitionend", transitionEndCallback);
+            item.innerHTML = lines[sel];
+            item.classList.toggle("fade");
+        }
+
+        item.addEventListener("transitionend", transitionEndCallback);
+        item.classList.toggle("fade");
     }
-    
-    item.addEventListener("transitionend", transitionEndCallback);
-    item.classList.toggle("fade");
-  }
 }
 
 setInterval(
-  function() {
-    pickAboutMe(ABOUT_ME_LINES, "aboutMeLine");
-  },
-  
-  // interval in milliseconds
-  30 * 1000
+    function () {
+        pickAboutMe(ABOUT_ME_LINES, "aboutMeLine");
+    },
+
+    // interval in milliseconds
+    30 * 1000
 );
 
